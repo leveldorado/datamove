ddb86cc034629794e7ba8c2e808944785fe0fc45
Vasyl <leveldorado@gmail.com>
2016-12-07 17:18:01 +0200

    Move databases

--- move/move.go
+++ move/move.go
@@ -33,6 +33,63 @@ func (m *Mover) MoveCollection(database, collection string) chan error {
 			doc = bson.M{}
 		}
 		ch <- iter.Close()
+		close(ch)
 	}()
 	return ch
+}
+
+func (m *Mover) MoveDatabase(database string) chan error {
+	ch := make(chan error, 1)
+	go func() {
+		db := m.source.DB(database)
+		collections, err := db.CollectionNames()
+		if err != nil {
+			ch <- err
+			close(ch)
+			return
+		}
+		chansList := make([]chan error, len(collections))
+		for _, col := range collections {
+			chansList = append(chansList, m.MoveCollection(database, col))
+		}
+		for _, v := range chansList {
+			if err := <-v; err != nil {
+				ch <- err
+			}
+		}
+		ch <- nil
+		close(ch)
+	}()
+	return ch
+}
+
+func (m *Mover) MoveDatabases(databases []string) chan error {
+	ch := make(chan error, 1)
+	go func() {
+		chansList := make([]chan error, len(databases))
+		for _, v := range databases {
+			chansList = append(chansList, m.MoveDatabase(v))
+		}
+		for _, c := range chansList {
+			for err := range c {
+				if err != nil {
+					ch <- err
+				}
+			}
+		}
+		ch <- nil
+		close(ch)
+	}()
+	return ch
+}
+
+func (m *Mover) MoveAllDatabases() chan error {
+	databases, err := m.source.DatabaseNames()
+	if err != nil {
+		ch := make(chan error, 1)
+		ch <- err
+		close(ch)
+		return ch
+	}
+	return m.MoveDatabases(databases)
 }
